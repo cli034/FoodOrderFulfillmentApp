8d3a707137f9e37ac75db444e40c06fa72aaf6a2
Adrian <li-adrian@hotmail.com>
2025-09-07 11:29:43 -0700

    remove unused model

--- app/src/main/java/com/example/csschallenge/data/model/OrderEventsResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.csschallenge.data.model
-
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class OrderEventsResponse(
-    val orderEvents: List<NetworkOrderEvent>
-)
--- app/src/main/java/com/example/csschallenge/data/service/OrderService.kt
+++ app/src/main/java/com/example/csschallenge/data/service/OrderService.kt
@@ -1,7 +1,6 @@
 package com.example.csschallenge.data.service
 
 import com.example.csschallenge.data.model.NetworkOrderEvent
-import com.example.csschallenge.data.model.OrderEventsResponse
 import retrofit2.http.GET
 
 interface OrderService {
